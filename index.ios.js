d128f810ec1c44ce7ec2ad3464adc641d56cdba4
Max Li <maxlivinci@gmail.com>
2016-08-21 19:46:50 +0800

    Flexbox continue

--- index.ios.js
+++ index.ios.js
@@ -6,12 +6,12 @@ class ReactNativeStopwatch extends Component {
     return (
       <View style={styles.container}>
         <View style={[styles.header, this.border('yellow')]}>
-          <View style={this.border('red')}>
+          <View style={[styles.timerWrapper, this.border('red')]}>
             <Text>
               00:00:00
             </Text>
           </View>
-          <View style={this.border('green')}>
+          <View style={[styles.buttonWrapper, this.border('green')]}>
             {this.startStopButton()}
             {this.lapButton()}
           </View>
@@ -64,6 +64,17 @@ const styles = StyleSheet.create({
   },
   footer: {
     flex: 1
+  },
+  timerWrapper: {
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonWrapper: {
+    flex: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
   }
 })
 
